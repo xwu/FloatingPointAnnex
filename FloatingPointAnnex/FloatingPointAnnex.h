b256ed451b652fee4dadefa3b8aa2b1d76128fe0
Xiaodi Wu <xiaodi.wu@gmail.com>
2018-01-06 13:09:10 -0600

    Initial Commit

---
+++ FloatingPointAnnex/FloatingPointAnnex.h
@@ -0,0 +1,19 @@
+//
+//  FloatingPointAnnex.h
+//  FloatingPointAnnex
+//
+//  Created by Xiaodi Wu on 1/6/18.
+//  Copyright © 2018 Xiaodi Wu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FloatingPointAnnex.
+FOUNDATION_EXPORT double FloatingPointAnnexVersionNumber;
+
+//! Project version string for FloatingPointAnnex.
+FOUNDATION_EXPORT const unsigned char FloatingPointAnnexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingPointAnnex/PublicHeader.h>
+
+
